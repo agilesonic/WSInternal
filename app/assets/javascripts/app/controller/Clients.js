81c73eb35de6ea46444c173d5afdcf5031f1da28
agilesonic <agilesonic@gmail.com>
2013-01-13 19:47:37 +0000

    fixed list view

--- app/assets/javascripts/app/controller/Clients.js
+++ app/assets/javascripts/app/controller/Clients.js
@@ -1,12 +1,12 @@
 Ext.define('AM.controller.Clients', {
     extend: 'Ext.app.Controller',
-/*
+
     stores: ['Clients'],
 
     models: ['Client'],
 
     views: ['clients.List'],
-*/
+
     init: function() {
 //    	this.getClientsStore().load();
     	alert('loaded');
--- app/assets/javascripts/app/view/clients/List.js
+++ app/assets/javascripts/app/view/clients/List.js
@@ -1,13 +1,16 @@
-Ext.define('AM.view.client.List' ,{
+Ext.define('AM.view.clients.List', {
     extend: 'Ext.grid.Panel',
-    alias : 'widget.clientlist',
+    alias: 'widget.clientlist',
 
     title : 'Clients',
     store: 'Clients',
 
     columns: [
+    
         {header: 'First Name',  dataIndex: 'firstname',  flex: 1},
         {header: 'Last Name',  dataIndex: 'lastname',  flex: 1},
         {header: 'Email', dataIndex: 'email', flex: 1}
+        
     ]
+    
 });
